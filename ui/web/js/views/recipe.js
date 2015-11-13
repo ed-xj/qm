f45f1618e0ecb1a2e94b958fe726056ec1b07ebc
Baocai Zheng <zhengbaocai1981@yahoo.co.jp>
2015-11-13 17:20:45 +0900

    add check all / check none feature to recipe view

--- ui/web/js/views/recipe.js
+++ ui/web/js/views/recipe.js
@@ -4,13 +4,45 @@ window.RecipeView = window.BaseView.extend({
     },
 
     events : {
-        "click #save":"saveBtnCLick",               // read it
+        "click .chk-all":"chkAllCLick",               // check all
+        "click .chk-none":"chkNoneCLick",               // check none
+        "click .chk-element":"chkElemCLick",               // check element
+        "click #save":"saveBtnCLick",               // save
     },
 
     render: function () {
         $(this.el).html(this.template());
         return this;
     },
+
+    chkAllCLick:function (e) {
+        var chkGroup = $(e.currentTarget).parents(".chk-group:first");
+        chkGroup.find(".chk-element").prop("checked", "checked");
+        chkGroup.find(".chk-none").prop("checked", "");
+        chkGroup.find(".chk-all").prop("checked", "checked");
+    },
+
+    chkNoneCLick:function (e) {
+        var chkGroup = $(e.currentTarget).parents(".chk-group:first");
+        chkGroup.find(".chk-element").prop("checked", "");
+        chkGroup.find(".chk-all").prop("checked", "");
+        chkGroup.find(".chk-none").prop("checked", "checked");
+    },
+
+    chkElemCLick:function (e) {
+        var chkGroup = $(e.currentTarget).parents(".chk-group:first");
+        if (chkGroup.find(".chk-element:checked").length == chkGroup.find(".chk-element").length) {
+            chkGroup.find(".chk-all").prop("checked", "checked");
+            chkGroup.find(".chk-none").prop("checked", "");
+        } else if (chkGroup.find(".chk-element:checked").length == 0) {
+            chkGroup.find(".chk-none").prop("checked", "checked");
+            chkGroup.find(".chk-all").prop("checked", "");
+        } else {
+            chkGroup.find(".chk-all").prop("checked", "");
+            chkGroup.find(".chk-none").prop("checked", "");
+        }
+    },
+
 
     ajaxUrl: "/cgi-bin/tcp_socket_client.js",
 
